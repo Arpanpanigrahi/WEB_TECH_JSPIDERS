51e1ac3cba737ba3af195b8e63eedd8e5c059001
ARPAN PANIGRAHI <46858538+Arpanpanigrahi@users.noreply.github.com>
2023-08-11 02:18:27 +0530

    Add files via upload

---
+++ JAVA SCRIPT/3. Operators/script.js
@@ -0,0 +1,23 @@
+const firstNumber = false;
+const secondNumber = true;
+
+// const result = firstNumber + secondNumber;
+// const result = firstNumber % secondNumber;
+const result = firstNumber || secondNumber;
+
+//console.log(!secondNumber);
+console.log(3 + 3);             //6
+console.log("3" + "3");        //33
+console.log("3" + + 3);        //33
+console.log(3 + + "3");        //6
+console.log("3" + "3" - "3");  //30
+
+
+// console.log(result);
+
+// 3 = 0*4 + 3
+
+// let thirdNumber = 12;
+// thirdNumber **= 4; // thirdNumber = thirdNumber + 4;
+
+// console.log(result);
